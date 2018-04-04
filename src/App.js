f664acfbb3e7a7909f03588fc35c48d3d9668d3f
MbF-Maria <rizwan-mbf-at-531339033506>
2018-04-04 12:39:51 +0530

    helloworld task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from './Home';
-import About from './About';
 import './App.css';
 
 class App extends Component {
@@ -16,22 +9,9 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <div className="App-homescreen">
-          <h1> Helloworld </h1>
-          <Router>
-              <div className="container">
-             <ul>
-               <li><Link to="/">Home</Link></li>
-               <li><Link to="/about">About</Link></li>
-             </ul>
-            <Route exact path="/" component={Home} /> 
-            <Route path="/about" component={About} /> 
-              </div>
-          </Router>
-        </div>
+          <h2> Helloworld </h2>
       </div>
     );
   }
 }
-
 export default App;
