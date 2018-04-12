4a439f203c4190a96582bb1b6026fa3c0e8fd12e
MbF-Maria <rizwan-mbf-at-531339033506>
2018-04-12 16:40:03 +0530

    Redux action my first try

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+
+function rootReducer(state = {}, action){
+  switch(action.type) {
+    case 'ADD-ONE':
+     return state.concat('added one')
+    case 'SUBTRACT-ONE':
+      return state.concat('subtracted one')
+    default:
+      return state
+  }
+}
+
+let store = createStore(rootReducer);
 
 class App extends Component {
+
+  constructor(){
+    super()
+    store.subscribe(() => {
+      console.log('subscribing to store');
+      console.log(store.getState())
+    });
+  }
+
+  increment() {
+    console.log('in store increment method');
+    store.dispatch({type:'INCREMENT', data:'added one'});
+  }
+
   render(){
     return(
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-          <h2> Helloworld </h2>
+        <h1>simple redux</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
       </div>
     );
   }
 }
+
 export default App;
--- src/component/Form/buttonprimary.js
+++ src/component/Form/buttonprimary.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
 
-const ButtonPrimary = ({ label }) => (
-  <button className="btn">
+const ButtonPrimary = ({ label, onClick }) => (
+  <button className="btn" onClick={onClick}>
     {label}
   </button>
 );
 
 ButtonPrimary.propTypes = {
   label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default ButtonPrimary;
