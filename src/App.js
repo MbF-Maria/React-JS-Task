59953fc609e8a113c64b8409d0d52407624df5f6
MbF-Maria <rizwan-mbf-at-531339033506>
2018-05-23 15:37:50 +0530

    fetch only id and fetch only single array of list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import map from 'lodash/map';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,37 +7,50 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = { 
-      title: '',
-      body:'',
+      details: {},
+      userId:'',
       id:'',
+      title: '',
+      body: '',
+      Subject: [],
     };
+    this.userId = this.userId.bind(this);
   }
 
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
       .then(data => {
-        this.setState({title:data.title,body:data.body,id:data.id})
-        console.log(data)
+        this.setState({title:data.title,body:data.body,id:data.id,userid:data.userId})
+        this.setState({Subject: data[99]})
+        console.log(this.state.Subject)
+        this.setState({details: data})
       });
+ }
+
+  userId(){
+    const listOfEmployee = [];
+    map(this.state.details, el => { listOfEmployee.push(el.id); });
+    console.log(listOfEmployee)
   }
 
   render() {
-    const { title,body,id } = this.state;
+    const { Subject } = this.state;
+    this.userId();
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div className="App-homescreen">
-          <h1> Title : {title}</h1>
-          <p>Subject : {body}</p>
-          <p>Id : {id}</p>
+          <h1> Name : {Subject.title}</h1>
+          <p>Detail : {Subject.body}</p>
+          <p>Id : {Subject.id}</p>
+          <p>Employee No : {Subject.userId}</p>
         </div>
       </div>
     );
   }
 }
-
 export default App;
